7511ae3818483abb335289c68b490f3f7ac2c735
Maijs Garais <maireadsbooks@gmail.com>
2022-11-25 10:30:27 +0000

    refactor player movement to reflect real world physics better, remove warnings for discarded returns

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const ACCELERATION: = 10
+const ACCELERATION: = 500
 const MAX_SPEED: = 100
-const FRICTION: = 15
+const FRICTION: = 500
 
 var velocity = Vector2.ZERO
 
@@ -12,11 +12,8 @@ func _physics_process(delta: float) -> void:
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
-		# normalise to prevent faster diagonal movement
-		velocity += input_vector.normalized() * ACCELERATION * delta
-		# cap velocity
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta)
