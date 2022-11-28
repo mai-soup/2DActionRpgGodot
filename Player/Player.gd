6fbc9fe93108d3164faa6c7ae967b3912e625d00
Maijs Garais <maireadsbooks@gmail.com>
2022-11-28 22:16:04 +0000

    add attack animation, state machine for player

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,9 +8,25 @@ const ACCELERATION: = 500
 const MAX_SPEED: = 80
 const FRICTION: = 500
 
-var velocity = Vector2.ZERO
+enum { MOVE, ROLL, ATTACK }
+
+var state: = MOVE
+var velocity: = Vector2.ZERO
+
+func _ready() -> void:
+	# only activate animation tree when the game is started
+	animTree.active = true
 
 func _physics_process(delta: float) -> void:
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			roll_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta: float) -> void:
 	var input_vector: = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("move_right") - \
 		Input.get_action_strength("move_left")
@@ -21,6 +37,7 @@ func _physics_process(delta: float) -> void:
 		# set up blend positions for both animations
 		animTree.set("parameters/Idle/blend_position", input_vector)
 		animTree.set("parameters/Run/blend_position", input_vector)
+		animTree.set("parameters/Attack/blend_position", input_vector)
 		# play the run animations
 		animState.travel("Run")
 		# accelerate
@@ -33,3 +50,16 @@ func _physics_process(delta: float) -> void:
 	
 	# reassign velocity from move and slide's remnant after collision	
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(_delta: float) -> void:
+	velocity = Vector2.ZERO
+	animState.travel("Attack")
+
+func roll_state(_delta: float) -> void:
+	pass
+	
+func attack_anim_finished() -> void:
+	state = MOVE
