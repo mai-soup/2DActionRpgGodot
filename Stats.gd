21d37842067d1bd887cd5f37f6dabda26ff991f5
Maijs Garais <maireadsbooks@gmail.com>
2022-12-06 09:03:49 +0000

    finish health ui with hearts

--- Stats.gd
+++ Stats.gd
@@ -2,9 +2,10 @@ extends Node
 
 signal no_health
 signal health_changed(value)
+signal max_health_changed(value)
 
-export var max_health: = 1
-onready var health: = max_health setget set_health
+export var max_health: = 1 setget set_max_health
+var health: = max_health setget set_health
 
 func set_health(value: int) -> void:
 	health = value
@@ -12,3 +13,11 @@ func set_health(value: int) -> void:
 	
 	if health <= 0:
 		emit_signal("no_health")
+
+func set_max_health(value: int) -> void:
+	max_health = value
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
+
+func _ready() -> void:
+	self.health = max_health
--- UI/HealthUI.gd
+++ UI/HealthUI.gd
@@ -1,6 +1,9 @@
 extends Control
 
-onready var label = $Label
+onready var heart_ui_full: = $HeartUIFull
+onready var heart_ui_empty: = $HeartUIEmpty
+
+const HEART_SIZE: = 15
 
 var hearts: = 4 setget set_hearts
 var max_hearts: = 4 setget set_max_hearts
@@ -8,14 +11,19 @@ var max_hearts: = 4 setget set_max_hearts
 func set_hearts(value: int) -> void:
 	hearts = clamp(value, 0, max_hearts)
 	
-	if label != null:
-		label.text = "HP = " + str(hearts)
+	if heart_ui_full != null:
+		heart_ui_full.rect_size.x = hearts * HEART_SIZE
 
 func set_max_hearts(value: int) -> void:
 	# max hearts can never be < 0
 	max_hearts = max(value, 1)
+	self.hearts = min(hearts, max_hearts)
+	
+	if heart_ui_empty != null:
+		heart_ui_empty.rect_size.x = max_hearts * HEART_SIZE
 
 func _ready() -> void:
 	self.max_hearts = PlayerStats.max_health
 	self.hearts = PlayerStats.health
 	PlayerStats.connect("health_changed", self, "set_hearts")
+	PlayerStats.connect("max_health_changed", self, "set_max_hearts")
